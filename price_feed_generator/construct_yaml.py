6fcde40e7d39bb0c270c9e1778b3b6ec1bf3d542
Ben Smith <bh2smith@gmail.com>
2021-11-03 20:07:45 +0100

    cleanup

--- price_feed_generator/construct_yaml.py
+++ price_feed_generator/construct_yaml.py
@@ -9,7 +9,9 @@ def load_coins(filepath: str) -> dict[str, list[dict]]:
         entries = json.loads(file.read())
         res = defaultdict(list)
         for entry in entries:
-            res[entry['symbol']].append(entry)
+            if entry['type'] == 'token':
+                # only include ethereum tokens
+                res[entry['symbol']].append(entry)
         return res
 
 
@@ -19,26 +21,26 @@ def load_tokens(filepath: str):
 
 
 if __name__ == "__main__":
-    coins = load_coins('data/coinpaprika.json')
+    coins = load_coins('data/coin-paprika.json')
     tokens = load_tokens('data/tokens-by-popularity.csv')
-    
+
     tokens.sort(key=lambda entry: int(entry['popularity']), reverse=True)
 
     found, output = 0, ""
     for token in tokens:
         if token['symbol'] in coins:
-            possible_tokens = [coin for coin in coins[token['symbol']] if coin['type'] != 'coin']
+            possible_tokens = coins[token['symbol']]
             if len(possible_tokens) == 1:
                 possible_token = possible_tokens[0]
-                if possible_token['is_active'] == True and possible_token['is_new'] == False:
+                if possible_token['is_active'] and not possible_token['is_new']:
                     # dune uses the snake case id as the name
                     token_name = possible_token['id'].replace('-', '_')
+                    # TODO - use real object and dump to yaml with yaml library
                     output += f"- name: {token_name}\n  id: {possible_token['id']}\n  symbol: {token['symbol']}\n  address: {token['address']}\n  decimals: {token['decimals']}\n"""
                     found += 1
             else:
-                print("Token not uniquely identifyable!", token, len(possible_tokens))
+                print("Token not uniquely identifiable!", token, len(possible_tokens))
         if found > 50:
             break
     with open('./data/reduced-output.yaml', 'w') as out_file:
         out_file.write(output)
-    
