e1b4bef37f9e7120ac84b8e1d9fbdcbfc85b12ab
Ben Smith <bh2smith@gmail.com>
2021-11-04 12:12:09 +0100

    use yaml output dumper

--- price_feed_generator/construct_yaml.py
+++ price_feed_generator/construct_yaml.py
@@ -5,15 +5,34 @@ from collections import defaultdict
 import yaml
 
 
+# Begin Hack
+# This little block is a hack to get the address (as a number) to print without quotes.
+class HexInt(int):
+    pass
+
+
+def representer(dumper, data):
+    result = yaml.ScalarNode(
+        'tag:yaml.org,2002:int',
+        '0x{:040x}'.format(data)
+    )
+    return result
+
+
+yaml.add_representer(HexInt, representer)
+
+
+# End Hack
+
 def load_coins(filepath: str) -> dict[str, list[dict]]:
     with open(filepath, 'r') as file:
         entries = json.loads(file.read())
-        res = defaultdict(list)
+        coin_dict = defaultdict(list)
         for entry in entries:
             if entry['type'] == 'token' and entry['is_active'] and not entry['is_new']:
                 # only include ethereum tokens
-                res[entry['symbol']].append(entry)
-        return res
+                coin_dict[entry['symbol']].append(entry)
+        return coin_dict
 
 
 def load_tokens(filepath: str) -> list[dict]:
@@ -30,21 +49,18 @@ if __name__ == "__main__":
         reverse=True
     )
 
-    found, output, res = 0, "", []
+    found, res = 0, []
     for token in tokens:
         if token['symbol'] in coins:
             possible_tokens = coins[token['symbol']]
             if len(possible_tokens) == 1:
                 coin_id = possible_tokens[0]['id']
-                # dune uses the snake case id as the name
-                name = coin_id.replace('-', '_')
-                # TODO - use real object and dump to yaml with yaml library
-                output += f"- name: {name}\n  id: {coin_id}\n  symbol: {token['symbol']}\n  address: {token['address']}\n  decimals: {token['decimals']}\n"""
                 res.append({
-                    'name': name,
+                    # dune uses the snake case id as the name
+                    'name': coin_id.replace('-', '_'),
                     'id': coin_id,
                     'symbol': token['symbol'],
-                    'address': token['address'],
+                    'address': HexInt(int(token['address'], 16)),
                     'decimals': int(token['decimals']),
                 })
                 found += 1
@@ -52,9 +68,6 @@ if __name__ == "__main__":
                 print("Token not uniquely identifiable!", token, len(possible_tokens))
         if found > 50:
             break
-    with open('./data/reduced-output.yaml', 'w') as out_file:
-        out_file.write(output)
 
-    # TODO - the problem with this is that the token address has quotes on the output.
-    with open('./data/result.yml', 'w') as yaml_file:
-        yaml.dump(res, yaml_file, default_flow_style=False)
+    with open('./data/result.yaml', 'w') as yaml_file:
+        yaml.dump(res, yaml_file, default_flow_style=False, sort_keys=False)
