043c8a0f37daa01eb5652cb734b9ed00d54698c2
Ben Smith <bh2smith@gmail.com>
2021-11-04 09:51:31 +0100

    attempt to yaml.dump

--- price_feed_generator/construct_yaml.py
+++ price_feed_generator/construct_yaml.py
@@ -1,7 +1,8 @@
 import csv
 import json
-
 from collections import defaultdict
+
+import yaml
 
 
 def load_coins(filepath: str) -> dict[str, list[dict]]:
@@ -9,38 +10,51 @@ def load_coins(filepath: str) -> dict[str, list[dict]]:
         entries = json.loads(file.read())
         res = defaultdict(list)
         for entry in entries:
-            if entry['type'] == 'token':
+            if entry['type'] == 'token' and entry['is_active'] and not entry['is_new']:
                 # only include ethereum tokens
                 res[entry['symbol']].append(entry)
         return res
 
 
-def load_tokens(filepath: str):
+def load_tokens(filepath: str) -> list[dict]:
     with open(filepath, newline='') as file:
         return list(csv.DictReader(file))
 
 
 if __name__ == "__main__":
     coins = load_coins('data/coin-paprika.json')
-    tokens = load_tokens('data/tokens-by-popularity.csv')
+    # Fetch tokens orders by descending, popularity
+    tokens = sorted(
+        load_tokens('data/tokens-by-popularity.csv'),
+        key=lambda entry: int(entry['popularity']),
+        reverse=True
+    )
 
-    tokens.sort(key=lambda entry: int(entry['popularity']), reverse=True)
-
-    found, output = 0, ""
+    found, output, res = 0, "", []
     for token in tokens:
         if token['symbol'] in coins:
             possible_tokens = coins[token['symbol']]
             if len(possible_tokens) == 1:
-                possible_token = possible_tokens[0]
-                if possible_token['is_active'] and not possible_token['is_new']:
-                    # dune uses the snake case id as the name
-                    token_name = possible_token['id'].replace('-', '_')
-                    # TODO - use real object and dump to yaml with yaml library
-                    output += f"- name: {token_name}\n  id: {possible_token['id']}\n  symbol: {token['symbol']}\n  address: {token['address']}\n  decimals: {token['decimals']}\n"""
-                    found += 1
+                coin_id = possible_tokens[0]['id']
+                # dune uses the snake case id as the name
+                name = coin_id.replace('-', '_')
+                # TODO - use real object and dump to yaml with yaml library
+                output += f"- name: {name}\n  id: {coin_id}\n  symbol: {token['symbol']}\n  address: {token['address']}\n  decimals: {token['decimals']}\n"""
+                res.append({
+                    'name': name,
+                    'id': coin_id,
+                    'symbol': token['symbol'],
+                    'address': token['address'],
+                    'decimals': int(token['decimals']),
+                })
+                found += 1
             else:
                 print("Token not uniquely identifiable!", token, len(possible_tokens))
         if found > 50:
             break
     with open('./data/reduced-output.yaml', 'w') as out_file:
         out_file.write(output)
+
+    # TODO - the problem with this is that the token address has quotes on the output.
+    with open('./data/result.yml', 'w') as yaml_file:
+        yaml.dump(res, yaml_file, default_flow_style=False)
